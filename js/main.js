34c72415fdabc690925450ea7b884477fd7345d3
abbos <obboshon22@gmail.com>
2022-06-03 23:43:33 +0500

    project commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var firstName = prompt ('What is your name');
+var firstQuestion = prompt ('What is Bootstrap');
+var secondQuestion = prompt ('When did Bootstrap create');
+var thirdQuestion = prompt ('Who created Bootstrap');
+var fourthQuestion = prompt ('What is breakpoints in bootstrap');
+var fifthQuestion = prompt ('What is expanded name of CSS');
+
+console.log(firstName + '\n' + firstQuestion + '\n' + secondQuestion + '\n' + thirdQuestion + '\n' + fourthQuestion + '\n' + fifthQuestion ) ;
+
